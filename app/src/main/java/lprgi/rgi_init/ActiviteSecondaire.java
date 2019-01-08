d9f528bea8469c5acdbc4c3905b9f7fe2f5f9771
remy lefevre <remy.lefevre@etud.u-picardie.fr>
2019-01-08 17:05:02 +0100

    ajout de lien entre activité

--- app/src/main/java/lprgi/rgi_init/ActiviteSecondaire.java
+++ app/src/main/java/lprgi/rgi_init/ActiviteSecondaire.java
@@ -1,13 +1,59 @@
 package lprgi.rgi_init;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActiviteSecondaire extends AppCompatActivity {
+
+    //on créer un objet de type button
+    private Button monBoutonQuitter;
+
+    private TextView montextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activite_secondaire);
+
+        //on place le widget dans notre variable grâce à son id
+        monBoutonQuitter = findViewById(R.id.id_btn_quitter);
+
+        montextView = findViewById(R.id.id_txtview);
+
+        //===============================================
+        //   Recuperer les infos d'une autre activité
+        //===============================================
+        //va recuperer l'intent qui m'a appelé
+        Intent unIntent=getIntent();
+
+        Bundle sonBundle=unIntent.getExtras();
+
+        int i= sonBundle.getInt("clef_de_entier");
+        CharSequence cq = sonBundle.getCharSequence("clef_de_chaine");
+
+        montextView.setText(i + " - " + cq);
+
+
+        //======================================================
+        //                      Revenir en arriere
+        //======================================================
+        /*
+        //méthode qui permet de revenir sur l'écran précedent
+        monBoutonQuitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });*/
+
+        //version pour java 8
+        monBoutonQuitter.setOnClickListener(v->{
+
+            finish();
+        });
     }
 }
--- app/src/main/java/lprgi/rgi_init/Activite_Principale.java
+++ app/src/main/java/lprgi/rgi_init/Activite_Principale.java
@@ -1,10 +1,16 @@
 package lprgi.rgi_init;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activite_Principale extends AppCompatActivity {
+
+    private Button boutonAvecRetour;
+    private TextView textViewResultat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +18,35 @@ public class Activite_Principale extends AppCompatActivity {
 
         /*fait reference à la ressource situé dans le dossier res (dans notre cas layout/layout...)*/
         setContentView(R.layout.layout_activite__principale);
+
+        boutonAvecRetour = findViewById(R.id.id_btn_avec_retour);
+
+        textViewResultat = findViewById(R.id.id_txtview_rslt);
+
+        //lorsque l'on clique avec le bouton
+        boutonAvecRetour.setOnClickListener(v->{
+
+        });
     }
 
     //méthode créer automatique depuis le onclick xml
     public void LancerActivite(View view) {
 
+        //on instancie un intent avec la classe que l'on veut déclencher
+        Intent monIntent = new Intent(this, ActiviteSecondaire.class);
+
+        //==============================================
+        // Bundle : conteneur de donnée clé/valeur
+        //==============================================
+
+        Bundle monBundle = new Bundle();
+
+        monBundle.putInt("clef_de_entier", 123);
+        monBundle.putCharSequence("clef_de_chaine", "Malecifient");
+
+        monIntent.putExtras(monBundle);
+
+        //si tu l'a connais déclenche là
+        startActivity(monIntent);
     }
 }
